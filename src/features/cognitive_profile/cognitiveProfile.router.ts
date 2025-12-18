87a79188a2d07e14b17ae44396b48a7d4f831345
ProgrammerSnehasish <programmer.snehasishd900@gmail.com>
2025-12-18 19:26:11 +0530

    New Updates.

--- src/features/cognitive_profile/cognitiveProfile.router.ts
+++ src/features/cognitive_profile/cognitiveProfile.router.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { getProfile } from "./cognitiveProfile.controller";
 
 import { verifyToken } from "../../middlewares/jwtVerifiction";
+import { enforceTeacherOrStudent } from "../../middlewares/enforceTeacherorStudent";
 
 const cognitiveProfileRouter = express.Router();
 
-cognitiveProfileRouter.get("/user/:userId", verifyToken, getProfile);
+cognitiveProfileRouter.get("/user/:userId", verifyToken, enforceTeacherOrStudent, getProfile);
 
 export default cognitiveProfileRouter;
